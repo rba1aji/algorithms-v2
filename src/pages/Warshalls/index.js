0c70e71db0720ae39cc4f9a92be17fd6eee4a5d3
rba1aji <rjibalaji@gmail.com>
2022-06-19 14:38:06 +0530

    1 file modified

--- src/pages/Warshalls/index.js
+++ src/pages/Warshalls/index.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 // import InputArea from '../../components/InputArea';
+
+// const [input, setInput] = useState(0);
+const input=createContext();
+
 export default function warshalls() {
   return (
     <>
       <h1>Warshall's algorithm</h1>
       <label>Enter an ADJACENCY MATRIX</label>
       <InputBox />
+      <ShowInput />
     </>
   );
 }
 
-const inputContext = createContext();
+function ShowInput(){
+  return(
+    <p>{useContext(input)}</p>
+  );
+}
 
 function InputBox() {
+  function getInput(event) {
+    // useEffect(()=>{
+      <input.Provider value={event.target.value}>
+        <ShowInput/>
+      </input.Provider>
+    // });
+  }
   return (
     <>
-      <textarea rows={7} cols={28} placeholder={``} />;
+      <textarea rows={7} cols={28} placeholder={``} onChange={getInput} />
     </>
   );
 }
