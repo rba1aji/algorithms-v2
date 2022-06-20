188f557d5bf4b18eb92a1e3b915e8f3ce469d4fb
rba1aji <rjibalaji@gmail.com>
2022-06-20 16:04:07 +0530

    1 file modified

--- src/pages/Floyds/index.js
+++ src/pages/Floyds/index.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import { useState, createContext, useContext, useEffect } from 'react';
 export default function floyds() {
-  return <h2>imm Floyds from pages</h2>;
+  return (
+    <>
+      <h1>Floyd's algorithm</h1>
+      <InputBox />
+    </>
+  );
+}
+
+const [input, setInput] = useState('');
+function getInput(event) {
+  setInput(...input, event.target.value);
+}
+function InputBox() {
+  return <textarea rows={8} cols={25} onChange={getInput} />;
 }
