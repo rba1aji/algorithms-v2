6c1cd01a7341d502027e9d71ad032aae47730f4a
rba1aji <rjibalaji@gmail.com>
2022-06-20 17:28:08 +0530

    1 file modified

--- src/pages/Floyds/index.js
+++ src/pages/Floyds/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { useState, createContext, useContext, useEffect } from 'react';
-export default function floyds() {
-  const [input, setInput] = useState('');
 
-  function Calculate() {}
+export default function floyds() {
+
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState([[]]);
+
+  function Calculate() {
+    
+    const weightMatrix=String2WeightMatrix();
+
+    function String2WeightMatrix() {
+      
+    }
+
+  }
 
   function Demo() {
     setInput('0 inf 3 inf\n2 0 inf inf\ninf 7 0 1\n6 inf inf 0');
+    <Calculate />;
   }
 
   return (
     <>
       <h1>Floyd's algorithm</h1>
 
+      <label>Enter a WEIGHT MATRIX</label>
       <textarea
         rows={8}
         cols={25}
@@ -22,8 +35,9 @@ export default function floyds() {
         }}
       />
       <br />
-      <button onClick={Calculate}>Find solution</button>
+
       <button onClick={Demo}>Demo</button>
+      <button onClick={Calculate}>Find solution</button>
     </>
   );
 }
