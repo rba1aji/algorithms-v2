d6c134c07125a1ac3ad6f6b77caf4c58ab0a1a40
rba1aji <rjibalaji@gmail.com>
2022-06-20 16:56:28 +0530

    1 file modified

--- src/pages/Floyds/index.js
+++ src/pages/Floyds/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { useState, createContext, useContext, useEffect } from 'react';
 export default function floyds() {
+  const [input, setInput] = useState('');
+
+  function Calculate() {}
+
+  function Demo() {
+    setInput('0 inf 3 inf\n2 0 inf inf\ninf 7 0 1\n6 inf inf 0');
+  }
+
   return (
     <>
       <h1>Floyd's algorithm</h1>
-      <InputBox />
+
+      <textarea
+        rows={8}
+        cols={25}
+        value={input}
+        onChange={(event) => {
+          setInput(event.target.value);
+        }}
+      />
+      <br />
+      <button onClick={Calculate}>Find solution</button>
+      <button onClick={Demo}>Demo</button>
     </>
   );
-}
-
-const [input, setInput] = useState('');
-function getInput(event) {
-  setInput(...input, event.target.value);
-}
-function InputBox() {
-  return <textarea rows={8} cols={25} onChange={getInput} />;
 }
