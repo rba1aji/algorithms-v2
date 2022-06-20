601e6c72d471a0b57aa10b44b6a94e152dfb4e98
rba1aji <rjibalaji@gmail.com>
2022-06-20 18:00:58 +0530

    1 file modified

--- src/pages/Floyds/index.js
+++ src/pages/Floyds/index.js
@@ -2,23 +2,49 @@ import React from 'react';
 import { useState, createContext, useContext, useEffect } from 'react';
 
 export default function floyds() {
-
   const [input, setInput] = useState('');
   const [output, setOutput] = useState([[]]);
 
   function Calculate() {
-    
-    const weightMatrix=String2WeightMatrix();
+    const weightMatrix = String2WeightMatrix();
 
+    // alert(weightMatrix);
     function String2WeightMatrix() {
-      
-    }
+      if (input[0] == '\n') setInput(input.replace('\n', ''));
 
+      const weightMatrix1D = input
+        .replaceAll('\n', ' ')
+        .replaceAll(/ +/g, ' ')
+        .split(' ');
+      //last character need to remove ws
+
+      let n = Math.sqrt(weightMatrix1D.length);
+      let weightMatrix = [];
+      let k = 0;
+      for (let i = 0; i < n; i++) {
+        let tempArr = [];
+        for (let j = 0; j < n; j++) {
+          if (weightMatrix1D[k] == 'inf') tempArr.push(Number.MAX_VALUE);
+          else {
+            let temp = parseInt(weightMatrix1D[k]);
+            if (Number.isNaN(temp)) {
+              alert(
+                'ENTER VALID WEIGHT MATRIX\ntip:\nmake sure you put inf for infinity\nweight matrix must be square matrix\ntry removing white spaces after last element'
+              );
+              return;
+            } else tempArr.push(temp);
+          }
+          k++;
+        }
+        weightMatrix.push(tempArr);
+      }
+      return weightMatrix;
+    }
   }
 
   function Demo() {
     setInput('0 inf 3 inf\n2 0 inf inf\ninf 7 0 1\n6 inf inf 0');
-    <Calculate />;
+    // Calculate();
   }
 
   return (
